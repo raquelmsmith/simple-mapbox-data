a6dce27c5acd9cf564057c84b061cecb5fab43c5
Raquel Smith <raquel@foodbloggerpro.com>
2017-04-16 16:30:48 -0700

    Rename, add meta boxes for year and location

--- mapbox-data-for-wordpress.php
+++ mapbox-data-for-wordpress.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	Plugin Name: WP Mapbox
+	Plugin Name: Mapbox Data for WordPress
 	Plugin URI: TBD
 	Description: A plugin to manage Mapbox data in a WordPress install
 	Version: 0.0.0
@@ -9,13 +9,13 @@
 	License: GPL2
 */
 
-add_action( 'init', 'wp_mapbox_init' );
+add_action( 'init', 'mapbox_data_init' );
 
 /**
 	* Register a Map Data post type.
 */
 
-function wp_mapbox_init() {
+function mapbox_data_init() {
 	$labels = array(
 		'name'					=> _x( 'Map Data', 'post type general name', 'your-plugin-textdomain' ),
 		'singular_name'			=> _x( 'Map Data Point', 'post type singular name', 'your-plugin-textdomain' ),
@@ -44,14 +44,46 @@ function wp_mapbox_init() {
 		'has_archive'			=> true,
 		'Hierarchical'			=> false,
 		'menu_position'			=> 5,
+		'show_in_rest'			=> true,
 		'supports'				=> array( 
 			'title', 
 			'editor', 
 			'author', 
-			'thumbnail', 
-			'excerpt', 
-			'comments', 
+			'thumbnail',  
 			'revisions' )
 	);
-	register_post_type( 'map-data-point', $args );
+	register_post_type( 'map_data_point', $args );
+}
+
+/**
+ * Add meta boxes for Year and Location
+ *
+ * @param post $post The post object
+ * @link https://codex.wordpress.org/Plugin_API/Action_Reference/add_meta_boxes
+ */
+function map_data_point_add_meta_boxes( $post ){
+	add_meta_box( 'map_data_point_year', 'Year', 'map_data_point_year_build_meta_box', 'map_data_point', 'side', 'low' );
+	add_meta_box( 'map_data_point_location', 'Location', 'map_data_point_location_build_meta_box', 'map_data_point', 'normal', 'low' );
+}
+add_action( 'add_meta_boxes_map_data_point', 'map_data_point_add_meta_boxes' );
+
+/**
+ * Build custom field meta boxes for Year and Location
+ *
+ * @param post $post The post object
+ */
+function map_data_point_year_build_meta_box( $post ){
+	wp_nonce_field( basename( __FILE__ ), 'map_data_point_meta_box_nonce' );
+	$mapDataPoint_Year = get_post_meta( $post->ID, '_map_data_point_year' );
+	?>
+	<input type="text" name="year" value="<?php echo $mapDataPoint_Year; ?>" /> 
+	<?php
+}
+
+function map_data_point_location_build_meta_box( $post ){
+	wp_nonce_field( basename( __FILE__ ), 'map_data_point_meta_box_nonce' );
+	$mapDataPoint_Location = get_post_meta( $post->ID, '_map_data_point_location' );
+	?>
+	<input type="text" name="location" value="<?php echo $mapDataPoint_Location; ?>" /> 
+	<?php
 }
