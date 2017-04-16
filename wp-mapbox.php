969afa8fa7198ee3d5e57c50cd772895aae092d2
Raquel Smith <raquel@foodbloggerpro.com>
2017-04-15 20:56:33 -0700

    Initial commit

---
+++ wp-mapbox.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: WP Mapbox
+   Plugin URI: TBD
+   Description: A plugin to manage Mapbox data in a WordPress install
+   Version: 0.0.0
+   Author: Raquel M Smith
+   Author URI: http://raquelmsmith.com
+   License: GPL2
+   */
+?>
