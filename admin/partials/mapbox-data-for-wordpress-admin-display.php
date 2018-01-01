d7876398d37b9e5b4d33385e96a0e04cd9d6ae54
Raquel Smith <raquel@foodbloggerpro.com>
2018-01-01 13:36:55 -0800

    Sanitize / escape all data

--- admin/partials/mapbox-data-for-wordpress-admin-display.php
+++ admin/partials/mapbox-data-for-wordpress-admin-display.php
@@ -15,56 +15,53 @@
 ?>
 
 <?php
-		$custom_meta_fields = $this->create_custom_meta_fields_array();
-
-		// Use nonce for verification
-		echo '<input type="hidden" name="map_data_point_meta_box_nonce" value="'.wp_create_nonce('map_data_point_meta_box_nonce_value').'" />';
-	     
-	    // Begin the field table and loop
-	    echo '<table class="form-table">';
-	    foreach ($custom_meta_fields as $field) {
-	    	$field_id =  strtolower( preg_replace( "/\s/", "_", $field[ 'id' ] ) );
-	        // get value of this field if it exists for this post
-	        $meta = get_post_meta( get_the_ID() , '_' . $field_id , true);
-	        // begin a table row with
-	        echo '<tr>
-	                <th><label for="' . $field_id . '">' . $field['label'] . '</label></th>
-	                <td>';
-	                switch($field['type']) {
-	                    // case items will go here
-	                    // text
-						case 'text':
-						    echo '<input type="text" name="' . $field_id . '" id="' . $field_id . '" value="' . $meta . '" size="30" />
-						        <br />';
-						        if ( isset( $field['desc'] ) ) {
-						        	echo '<span class="description">' . $field['desc'] . '</span>';
-						        }
-							break;
-						// textarea
-						case 'textarea':
-						    echo '<textarea name="' . $field_id . '" id="' . $field_id . '" cols="60" rows="4">' . $meta . '</textarea>
-						        <br />';
-						        if ( isset( $field['desc'] ) ) {
-						        	echo '<span class="description">' . $field['desc'] . '</span>';
-						        }
-							break;
-						// checkbox
-						case 'checkbox':
-						    echo '<input type="checkbox" name="' . $field_id . '" id="' . $field_id .'" ',$meta ? ' checked="checked"' : '','/>
-						        <label for="' . $field_id . '">' . $field['desc'] . '</label>';
-							break;
-						// select
-						case 'select':
-						    echo '<select name="' . $field_id . '" id="' . $field_id . '">';
-						    foreach ($field['options'] as $option) {
-						        echo '<option', $meta == $option['value'] ? ' selected="selected"' : '', ' value="' . $option['value'] . '">' . $option['label'] . '</option>';
-						    }
-						    echo '</select><br />';
-						        if ( isset( $field['desc'] ) ) {
-						        	echo '<span class="description">' . $field['desc'] . '</span>';
-						        }
-							break;
-	                } //end switch
-	        echo '</td></tr>';
-	    } // end foreach
-	    echo '</table>'; // end table
+	$custom_meta_fields = $this->create_custom_meta_fields_array();
+?>
+	<input type="hidden" name="map_data_point_meta_box_nonce" value="<?php esc_attr_e( wp_create_nonce('map_data_point_meta_box_nonce_value') ); ?>" />
+     
+    <table class="form-table">
+    <?php foreach ($custom_meta_fields as $field) {
+    	$field_id =  strtolower( preg_replace( "/\s/", "_", $field[ 'id' ] ) );
+        // get value of this field if it exists for this post
+        $meta = get_post_meta( get_the_ID() , '_' . $field_id , true); ?>
+        <tr>
+            <th><label for="<?php esc_attr_e( $field_id ); ?>"><?php esc_html_e( $field['label'] ); ?></label></th>
+            <td>
+            <?php switch($field['type']) {
+                // text
+				case 'text':
+				    echo '<input type="text" name="' . esc_attr( $field_id ) . '" id="' . esc_attr( $field_id ) . '" value="' . esc_attr( $meta ) . '" size="30" />
+				        <br />';
+				        if ( isset( $field['desc'] ) ) {
+				        	echo '<span class="description">' . esc_html( $field['desc'] ) . '</span>';
+				        }
+					break;
+				// textarea
+				case 'textarea':
+				    echo '<textarea name="' . esc_attr( $field_id ) . '" id="' . esc_attr( $field_id ) . '" cols="60" rows="4">' . esc_html( $meta ) . '</textarea>
+				        <br />';
+				        if ( isset( $field['desc'] ) ) {
+				        	echo '<span class="description">' . esc_html( $field['desc'] ) . '</span>';
+				        }
+					break;
+				// checkbox
+				case 'checkbox':
+				    echo '<input type="checkbox" name="' . esc_attr( $field_id ) . '" id="' . esc_attr( $field_id ) .'" ',$meta ? ' checked="checked"' : '','/>
+				        <label for="' . esc_attr( $field_id ) . '">' . esc_html( $field['desc'] ) . '</label>';
+					break;
+				// select
+				case 'select':
+				    echo '<select name="' . esc_attr( $field_id ) . '" id="' . esc_attr( $field_id ) . '">';
+				    foreach ($field['options'] as $option) {
+				        echo '<option', $meta == $option['value'] ? ' selected="selected"' : '', ' value="' . $option['value'] . '">' . $option['label'] . '</option>';
+				    }
+				    echo '</select><br />';
+				        if ( isset( $field['desc'] ) ) {
+				        	echo '<span class="description">' . esc_html( $field['desc'] ) . '</span>';
+				        }
+					break;
+                } //end switch ?>
+        	</td>
+        </tr>
+    <?php } //end foreach ?>
+	</table>
--- admin/partials/mapbox-data-for-wordpress-options-display.php
+++ admin/partials/mapbox-data-for-wordpress-options-display.php
@@ -11,12 +11,12 @@
  */
 ?>
 
-<h2><?php _e( 'Mapbox Data for WordPress', 'wp_admin_style' ); ?></h2>
+<h2><?php _e( 'Mapbox Data for WordPress', 'mapbox-data-for-wordpress' ); ?></h2>
 
 <div class="wrap">
 
 	<div id="icon-options-general" class="icon32"></div>
-	<h1><?php esc_attr_e( 'Settings', 'wp_admin_style' ); ?></h1>
+	<h1><?php esc_attr_e( 'Settings', 'mapbox-data-for-wordpress' ); ?></h1>
 
 	<div id="poststuff">
 
@@ -29,43 +29,51 @@
 
 					<div class="postbox">
 
-						<h2><span><?php esc_attr_e( 'Mapbox Info', 'wp_admin_style' ); ?></span></h2>
+						<h2><span><?php esc_html_e( 'Mapbox Info', 'mapbox-data-for-wordpress' ); ?></span></h2>
 
 						<div class="inside">
-							<p><?php esc_attr_e(
+							<p><?php esc_html_e(
 									'We need this info to send data to your Mapbox account.',
-									'wp_admin_style'
+									'mapbox-data-for-wordpress'
 								); ?></p>
 							<form name="mdfw_mapbox_info_form" method="post" action="">
 								<input type="hidden" name="mdfw_mapbox_info_form_submitted" value="Y">
 								<table class="form-table">
 									<tr>
 										<td scope="row">
-											<label for="tablecell"><?php esc_attr_e('Mapbox Account Username', 'wp_admin_style'); ?></label>
+											<label for="tablecell"><?php esc_html_e('Mapbox Account Username', 'mapbox_data_for_wordpress'); ?></label>
 										</td>
 										<td>
-											<input type="text" name="mapbox_account_username" value="<?php echo $mapbox_account_username; ?>" class="regular-text" />
+											<input type="text" name="mapbox_account_username" value="<?php esc_attr_e( $mapbox_account_username ); ?>" class="regular-text" />
 										</td>
 									</tr>
 									<tr>
 										<td scope="row">
-											<label for="tablecell"><?php esc_attr_e('Mapbox Access Token', 'wp_admin_style'); ?></label>
+											<label for="tablecell"><?php esc_html_e('Mapbox Access Token', 'mapbox-data-for-wordpress'); ?></label>
 										</td>
 										<td>
-											<input type="text" name="mapbox_access_token" value="<?php echo $mapbox_access_token; ?>" class="regular-text" />
+											<input type="text" name="mapbox_access_token" value="<?php esc_attr_e( $mapbox_access_token ); ?>" class="regular-text" />
 										</td>
 									</tr>
 									<tr>
 										<td scope="row">
-											<label for="tablecell"><?php esc_attr_e('Mapbox Dataset ID', 'wp_admin_style'); ?></label>
+											<label for="tablecell"><?php esc_html_e('Mapbox Dataset ID', 'mapbox-data-for-wordpress'); ?></label>
 										</td>
 										<td>
-											<input type="text" name="mapbox_dataset_id" value="<?php echo $mapbox_dataset_id; ?>" class="regular-text" />
+											<input type="text" name="mapbox_dataset_id" value="<?php esc_attr_e( $mapbox_dataset_id ); ?>" class="regular-text" />
 										</td>
 									</tr>
 									<tr>
 										<td scope="row">
-											<label for="tablecell"><?php esc_attr_e('Send Categories', 'wp_admin_style'); ?></label>
+											<label for="tablecell"><?php esc_html_e('Mapbox Tileset ID', 'mapbox-data-for-wordpress'); ?></label>
+										</td>
+										<td>
+											<input type="text" name="mapbox_tileset_id" value="<?php esc_attr_e( $mapbox_tileset_id ); ?>" class="regular-text" />
+										</td>
+									</tr>
+									<tr>
+										<td scope="row">
+											<label for="tablecell"><?php esc_html_e('Send Categories', 'mapbox-data-for-wordpress'); ?></label>
 										</td>
 										<td>
 											<input type="checkbox" name="mdfw_send_categories" value="1" <?php checked( $mdfw_send_categories, '1', TRUE ); ?> />
@@ -73,7 +81,7 @@
 									</tr>
 									<tr>
 										<td scope="row">
-											<label for="tablecell"><?php esc_attr_e('Send Tags', 'wp_admin_style'); ?></label>
+											<label for="tablecell"><?php esc_html_e('Send Tags', 'mapbox-data-for-wordpress'); ?></label>
 										</td>
 										<td>
 											<input type="checkbox" name="mdfw_send_tags" value="1" <?php checked( $mdfw_send_tags, '1', TRUE ); ?> />
@@ -81,13 +89,13 @@
 									</tr>
 								</table>
 
-								<h2><span><?php esc_attr_e( 'Custom Fields', 'wp_admin_style' ); ?></span></h2>
+								<h2><span><?php esc_html_e( 'Custom Fields', 'mapbox-data-for-wordpress' ); ?></span></h2>
 
 								<table class="form-table">
 									<tr>
-										<th class="row-title"><?php esc_attr_e( 'Value Name', 'wp_admin_style' ); ?></th>
-										<th><?php esc_attr_e( 'Value Type', 'wp_admin_style' ); ?></th>
-										<th><?php esc_attr_e( 'Include in GeoJSON?', 'wp_admin_style' ); ?></th>
+										<th class="row-title"><?php esc_html_e( 'Value Name', 'mapbox-data-for-wordpress' ); ?></th>
+										<th><?php esc_html_e( 'Value Type', 'mapbox-data-for-wordpress' ); ?></th>
+										<th><?php esc_html_e( 'Include in GeoJSON?', 'mapbox-data-for-wordpress' ); ?></th>
 									</tr>
 
 									<?php 
@@ -99,25 +107,29 @@
 										<tr valign="top">
 
 										<td scope="row">
-											<input type="text" name="mdfw_custom_field_<?php echo $i; ?>" value="<?php echo $$field_name; ?>" class="regular-text" />
+											<input type="text" name="mdfw_custom_field_<?php esc_attr_e( $i ); ?>" value="<?php esc_attr_e( $$field_name ); ?>" class="regular-text" />
 										</td>
 										<td>
-											<select name="<?php echo $field_type; ?>" id="">
-												<option <?php if ( $$field_type == 'text' ) { echo 'selected="selected"'; } ?> value="text">Text</option>
-												<option <?php if ( $$field_type == 'textarea' ) { echo 'selected="selected"'; } ?> value="textarea">Textarea</option>
-												<option <?php if ( $$field_type == 'checkbox' ) { echo 'selected="selected"'; } ?> value="checkbox">Checkbox</option>
-												<option <?php if ( $$field_type == 'radio' ) { echo 'selected="selected"'; } ?> value="radio">Radio</option>
+											<select name="<?php esc_attr_e( $field_type ); ?>" id="">
+												<option <?php if ( $$field_type == 'text' ) : ?> selected="<?php esc_attr_e( 'selected' ); ?>"<?php endif; ?> value="text">Text</option>
+												<option <?php if ( $$field_type == 'textarea' ) : ?> selected="<?php esc_attr_e( 'selected' ); ?>"<?php endif; ?> value="textarea">Textarea</option>
+												<option <?php if ( $$field_type == 'checkbox' ) : ?> selected="<?php esc_attr_e( 'selected' ); ?>"<?php endif; ?> value="checkbox">Checkbox</option>
+												<option <?php if ( $$field_type == 'radio' ) : ?> selected="<?php esc_attr_e( 'selected' ); ?>"<?php endif; ?> value="radio">Radio</option>
 											</select>
 										</td>
 										<td>
-											<input type="checkbox" value="1" name="mdfw_custom_field_<?php echo $i; ?>_json" <?php checked( $$field_json, '1', TRUE ); ?> />
+											<input type="checkbox" value="1" name="mdfw_custom_field_<?php esc_attr_e( $i ); ?>_json" <?php checked( $$field_json, '1', TRUE ); ?> />
 										</td>
 									</tr>
 									<?php } ?>
 								</table>
 								<p>
 									<?php submit_button(
-										'Save', $type = 'primary', $name = 'mdfw-settings-submit', $wrap = FALSE, $other_attributes = NULL
+										'Save', 
+										$type = 'primary', 
+										$name = 'mdfw-settings-submit', 
+										$wrap = FALSE, 
+										$other_attributes = NULL
 									); ?>
 								</p>
 							</form>
@@ -140,14 +152,14 @@
 
 					<div class="postbox">
 
-						<h2><span><?php esc_attr_e(
-									'About Mapbox Data for WordPress', 'wp_admin_style'
+						<h2><span><?php esc_html_e(
+									'About Mapbox Data for WordPress', 'mapbox-data-for-wordpress'
 								); ?></span></h2>
 
 						<div class="inside">
-							<p><?php esc_attr_e(
+							<p><?php esc_html_e(
 									'Mapbox Data for WordPress allows you to manage your Mapbox data points from right within your WordPress installation.',
-									'wp_admin_style'
+									'mapbox-data-for-wordpress'
 								); ?></p>
 						</div>
 						<!-- .inside -->
@@ -157,19 +169,40 @@
 
 					<div class="postbox">
 
-						<h2><span><?php esc_attr_e(
-									'Send all existing data to Mapbox', 'wp_admin_style'
+						<h2><span><?php esc_html_e(
+									'Send all existing data to Mapbox', 'mapbox-data-for-wordpress'
 								); ?></span></h2>
 
 						<div class="inside">
-							<p><?php esc_attr_e(
+							<p><?php esc_html_e(
 									'If you have updated your Mapbox data in bulk or changed your settings, use the button below to send all existing data to Mapbox again. Do not close the page while the data is being updated.',
-									'wp_admin_style'
+									'mapbox-data-for-wordpress'
 								); ?></p>
 								<?php $nonce = wp_create_nonce("mdfw_update_all_nonce"); ?>
-								<a class="button-secondary mdfw-update-all" href="<?php echo admin_url('admin-ajax.php?action=mdfw_update_all&nonce='.$nonce); ?>" data-nonce="<?php echo $nonce; ?>"><?php esc_attr_e( 'Send all data to Mapbox' ); ?></a>
-								<p class="sending-data"><span class="dashicons dashicons-update loader"></span>Working... please leave the window open.</p>
-								<p class="data-sent"><span class="dashicons dashicons-yes"></span>Done! You may now close this window.</p>
+								<a class="button-secondary mdfw-update-all" href="<?php echo esc_url( admin_url( 'admin-ajax.php?action=mdfw_update_all&nonce=' . $nonce ) ); ?>" data-nonce="<?php esc_attr_e( $nonce, 'mapbox-data-for-wordpress' ); ?>"><?php esc_html_e( 'Send all data to Mapbox' ); ?></a>
+								<p class="sending-data"><span class="dashicons dashicons-update loader"></span><?php esc_html_e( 'Working... please leave the window open.', 'mapbox-data-for-wordpress' ); ?></p>
+								<p class="data-sent"><span class="dashicons dashicons-yes"></span><?php esc_html_e( 'Done! You may now close this window.', 'mapbox-data-for-wordpress' ); ?></p>
+						</div>
+						<!-- .inside -->
+
+					</div>
+					<!-- .postbox -->
+
+					<div class="postbox">
+
+						<h2><span><?php esc_html_e(
+									'Update Mapbox Tileset', 'mapbox-data-for-wordpress'
+								); ?></span></h2>
+
+						<div class="inside">
+							<p><?php esc_html_e(
+									'Update the Tileset from the Dataset. Clicking the below button will send the update request to Mapbox. After the request is sent, it may take a few minutes for the Mapbox servers to update the tileset.',
+									'mapbox-data-for-wordpress'
+								); ?></p>
+								<?php $nonce = wp_create_nonce("mdfw_update_tileset_nonce"); ?>
+								<a class="button-secondary mdfw-update-tileset" href="<?php echo esc_url( admin_url('admin-ajax.php?action=mdfw_update_tileset&nonce=' . $nonce ) ); ?>" data-nonce="<?php esc_attr_e( $nonce ); ?>"><?php esc_html_e( 'Update Tileset' ); ?></a>
+								<p class="sending-data"><span class="dashicons dashicons-update loader"></span><?php esc_html_e( 'Working... please leave the window open.', 'mapbox-data-for-wordpress' ); ?></p>
+								<p class="data-sent"><span class="dashicons dashicons-yes"></span><?php esc_html_e( 'The update request has been sent.', 'mapbox-data-for-wordpress' ); ?></p>
 						</div>
 						<!-- .inside -->
 
